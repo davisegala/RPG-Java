7ee48275c3a61517db49fe453246d431b2ae8cd3
Miojo Aligenigena <miojoaligenigena@gmail.com>
2026-05-21 10:58:23 -0300

    Add files via upload

---
+++ Entity/TypeStatus.java
@@ -0,0 +1,22 @@
+package Entity;
+
+public enum TypeStatus {
+    STRENGHT {
+        @Override
+        public int getValue(Entity e) { return e.getStrength(); }
+    },
+    SOURCERY {
+        @Override
+        public int getValue(Entity e) { return e.getSourcery(); }
+    },
+    RESISTANCE {
+        @Override
+        public int getValue(Entity e) { return e.getResistance(); }
+    },
+    AGILITY {
+        @Override
+        public int getValue(Entity e) { return e.getAgility(); }
+    };
+
+    public abstract int getValue(Entity e);
+}
